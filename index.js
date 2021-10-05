72cacee9722f66e2fb2040f4da9cd1e1e74ad856
Teja sri <tejasmadi1@gmail.com>
2021-10-05 10:21:19 +0000

    super over league

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div>
+    <h1 className="heading">Super Over League</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      className="size"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      className="size"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
